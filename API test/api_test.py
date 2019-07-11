3b1fc3249b3f2a6e775b90277ffaa5388560817c
Atif Mehmod <am02464@st.habib.edu.pk>
2019-07-11 06:10:34 -0700

    Search API integrated!!!

--- API test/api_test.py
+++ API test/api_test.py
@@ -1,7 +1,12 @@
 import requests as r
 
 
+app_key = "853be5bff9bae2b36c49b362524e0404"
+app_id = "d15954a8"
 
 
+endpoint = f"https://api.edamam.com/api/food-database/parser?ingr=red apple&app_id={app_id}&app_key={app_key}"
+print(endpoint)
 # Request
-request = r.get("https://google.com") 
+request = r.get(endpoint)
+print(request.json())
