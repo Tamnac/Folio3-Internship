0abea98eacc0b01b9f4aba30d70d9e9fa51b2481
Atif Mehmod <am02464@st.habib.edu.pk>
2019-07-17 13:30:33 +0500

    CROSS ORIGION SUPPORT ADDED

--- Backend/node-express/app.js
+++ Backend/node-express/app.js
@@ -17,6 +17,12 @@ const port = 8000
 const app = express()
 
 // * loading middleware 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use(appLevelMiddelware.requestLogger)
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
