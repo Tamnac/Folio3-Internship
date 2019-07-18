c9cbf6d76bbdfb9b7de40d3a2e8b82f51bc9c715
Mubahsra <50540817+mubashramajid@users.noreply.github.com>
2019-07-18 18:07:21 +0500

    Update weight.js

--- Backend/node-express/routes/weight.js
+++ Backend/node-express/routes/weight.js
@@ -1,6 +1,6 @@
 const express = require('express') // imported express
 const validator = require('validator')
-//copied from docs
+    //copied from docs
 var bodyParser = require('body-parser')
 var multer = require('multer') // v1.0.5
 var upload = multer() // for parsing multipart/form-data
@@ -14,6 +14,23 @@ let router = express.Router()
 // loading middleware
 router.use(authRequired)
 
+
+// * Weight Log endpoints start here
+router.post('/weight-log', upload.array(), (req, res) => {
+    /**
+     * logs wight of the currently logged In user 
+     */
+    // -> Atif 
+
+    //  let body = req.body
+    // let date = Date().now()
+    console.log(req.body)
+    db.run('insert into WeightLog(user_id, date, weight) values(?,?,?)', [req.user.id, '2019-07-08', req.body.weight],
+        function(err) {
+            res.send('done');
+
+        });
+})
 
 
 
